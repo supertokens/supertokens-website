bf5b955b822d714e89852e364de94fda5b0562e9
Rishabh Poddar <rishabh.poddar@gmail.com>
2019-05-11 00:01:43 +0530

    implements first version of onUnauthorised function

--- src/build/index.js
+++ src/build/index.js
@@ -1,4 +1,3 @@
-// import { acquireLock, releaseLock } from "browser-tabs-lock";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     return new (P || (P = Promise))(function (resolve, reject) {
         function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
@@ -7,8 +6,62 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-export function onUnauthorisedResponse() {
+import axios from 'axios';
+import Lock from 'browser-tabs-lock';
+const ID_COOKIE_NAME = "id-cookie-for-refresh-token";
+// You can modify the API call below as you like.
+// But be sure to use await
+function apiCall(REFRESH_TOKEN_URL) {
     return __awaiter(this, void 0, void 0, function* () {
-        // TODO:
+        let response = yield axios.post(REFRESH_TOKEN_URL, {});
+        return response;
     });
+}
+export function onUnauthorisedResponse(REFRESH_TOKEN_URL) {
+    return __awaiter(this, void 0, void 0, function* () {
+        let preLockID = getIDFromCookie();
+        if (preLockID === undefined) {
+            return { result: "SESSION_EXPIRED" };
+        }
+        let lock = new Lock();
+        if (yield lock.acquireLock("REFRESH_TOKEN_USE", 5000)) {
+            try {
+                let postLockID = getIDFromCookie();
+                if (postLockID === undefined) {
+                    return { result: "SESSION_EXPIRED" };
+                }
+                if (postLockID !== preLockID) {
+                    return { result: "RETRY" };
+                }
+                let response = yield apiCall(REFRESH_TOKEN_URL);
+                if (getIDFromCookie() === undefined) {
+                    return { result: "SESSION_EXPIRED" };
+                }
+                return { result: "SESSION_REFRESHED", response };
+            }
+            catch (err) {
+                // we consume error for clean interface. 
+            }
+            finally {
+                lock.releaseLock("REFRESH_TOKEN_USE");
+            }
+        }
+        if (getIDFromCookie() === undefined) {
+            return { result: "SESSION_EXPIRED" };
+        }
+        else {
+            return { result: "RETRY" };
+        }
+    });
+}
+function getIDFromCookie() {
+    let value = "; " + document.cookie;
+    let parts = value.split("; " + ID_COOKIE_NAME + "=");
+    if (parts.length === 2) {
+        let last = parts.pop();
+        if (last !== undefined) {
+            return last.split(";").shift();
+        }
+    }
+    return undefined;
 }
--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,5 +1,59 @@
-// import { acquireLock, releaseLock } from "browser-tabs-lock";
+import axios from 'axios';
+import Lock from 'browser-tabs-lock';
 
-export async function onUnauthorisedResponse() {
-    // TODO:
+const ID_COOKIE_NAME = "id-cookie-for-refresh-token"
+
+// You can modify the API call below as you like.
+// But be sure to use await
+async function apiCall(REFRESH_TOKEN_URL: string): Promise<any> {
+    let response = await axios.post(REFRESH_TOKEN_URL, {});
+    return response;
+}
+
+export async function onUnauthorisedResponse(REFRESH_TOKEN_URL: string):
+    Promise<{ result: "SESSION_EXPIRED" } |
+    { result: "SESSION_REFRESHED", response: any } |
+    { result: "RETRY" }> {
+    let preLockID = getIDFromCookie();
+    if (preLockID === undefined) {
+        return { result: "SESSION_EXPIRED" };
+    }
+    let lock = new Lock();
+    if (await lock.acquireLock("REFRESH_TOKEN_USE", 5000)) { // to sync across tabs.
+        try {
+            let postLockID = getIDFromCookie();
+            if (postLockID === undefined) {
+                return { result: "SESSION_EXPIRED" };
+            }
+            if (postLockID !== preLockID) {
+                return { result: "RETRY" };
+            }
+            let response = await apiCall(REFRESH_TOKEN_URL);
+            if (getIDFromCookie() === undefined) {  // removed by server. So we logout
+                return { result: "SESSION_EXPIRED" };
+            }
+            return { result: "SESSION_REFRESHED", response };
+        } catch (err) {
+            // we consume error for clean interface. 
+        } finally {
+            lock.releaseLock("REFRESH_TOKEN_USE");
+        }
+    }
+    if (getIDFromCookie() === undefined) {  // removed by server. So we logout
+        return { result: "SESSION_EXPIRED" };
+    } else {
+        return { result: "RETRY" };
+    }
+}
+
+function getIDFromCookie(): string | undefined {
+    let value = "; " + document.cookie;
+    let parts = value.split("; " + ID_COOKIE_NAME + "=");
+    if (parts.length === 2) {
+        let last = parts.pop();
+        if (last !== undefined) {
+            return last.split(";").shift();
+        }
+    }
+    return undefined;
 }
