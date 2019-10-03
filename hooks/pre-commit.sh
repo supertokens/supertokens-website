a394d51e4251b2dd6988916bcd4172ea9005bb58
Nemi Shah <nemishah1212@gmail.com>
2019-10-03 18:37:13 +0530

    Changes to pre commit hook

--- hooks/pre-commit.sh
+++ hooks/pre-commit.sh
@@ -75,3 +75,35 @@ else
    echo ""
    exit 2
 fi
+
+# get current version----------
+version=`cat package.json | grep -e '"version":'`
+while IFS="'" read -ra ADDR; do
+    counter=0
+    for i in "${ADDR[@]}"; do
+        if [ $counter == 3 ]
+        then
+            version=$i
+        fi
+        counter=$(($counter+1))
+    done
+done <<< "$version"
+# get git branch name-----------
+branch_name="$(git symbolic-ref HEAD 2>/dev/null)" ||
+branch_name="(unnamed branch)"     # detached HEAD
+branch_name=${branch_name##refs/heads/}
+# check if branch is correct based on the version-----------
+if [ $branch_name == "master" ]
+then
+    YELLOW='\033[1;33m'
+    NC='\033[0m' # No Color
+    printf "${YELLOW}committing to MASTER${NC}\n"
+    exit 0
+elif [[ $version == $branch_name* ]]
+then
+    exit 0
+fi
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+printf "${RED}Pushing to wrong branch. Stopping commit${NC}\n"
+exit 1
