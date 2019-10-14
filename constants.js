d0fd2dfaaf1df3bd58647a2e93f6ed6f0d3c0520
Nemi Shah <nemishah1212@gmail.com>
2019-10-14 19:34:51 +0530

    Bump version to 3.2.3

--- constants.js
+++ constants.js
@@ -1,2 +1,2 @@
-export var package_version = "3.2.2";
+export var package_version = "3.2.3";
 export var platform_name = "website";
--- constants.ts
+++ constants.ts
@@ -1,2 +1,2 @@
-export const package_version = "3.2.2";
+export const package_version = "3.2.3";
 export const platform_name = "website";
