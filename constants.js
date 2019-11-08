13aadec51b33e1bff49c776a666b2746feadbc9a
Nemi Shah <nemishah1212@gmail.com>
2019-11-08 17:24:22 +0530

    Bump version to 3.2.6

--- constants.js
+++ constants.js
@@ -1,4 +1,4 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.package_version = "3.2.5";
+exports.package_version = "3.2.6";
 exports.platform_name = "website";
--- constants.ts
+++ constants.ts
@@ -1,2 +1,2 @@
-export const package_version = "3.2.5";
+export const package_version = "3.2.6";
 export const platform_name = "website";
