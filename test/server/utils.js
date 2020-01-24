5d5da4830920f0cddc039e40ed0aa01adb35a3e0
Joel Coutinho <joelcoutinho@qually.io>
2020-01-24 19:56:30 +0530

    adds small change to testing

--- test/server/utils.js
+++ test/server/utils.js
@@ -88,7 +88,7 @@ module.exports.startST = async function(host = "localhost", port = 9000) {
             .executeCommand(
                 "cd " +
                     installationPath +
-                    ` && java -classpath "./core/*:./plugin-interface/*" io.supertokens.Main ./ DEV host=` +
+                    ` && java -Djava.security.egd=file:/dev/urandom -classpath "./core/*:./plugin-interface/*" io.supertokens.Main ./ DEV host=` +
                     host +
                     " port=" +
                     port
