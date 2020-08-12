206dd51b4060839cb695773fce3231f46a21d1f3
Rishabh Poddar <rishabh.poddar@gmail.com>
2020-08-12 16:34:16 +0530

    changes to allow for a different default refresh API path

--- test/server/index.js
+++ test/server/index.js
@@ -66,6 +66,7 @@ app.post("/beforeeach", async (req, res) => {
     await setupST();
     await setKeyValueInConfig("cookie_domain", '"localhost.org"');
     await setKeyValueInConfig("cookie_secure", "false");
+    await setKeyValueInConfig("refresh_api_path", "/refresh");
     res.send();
 });
 
