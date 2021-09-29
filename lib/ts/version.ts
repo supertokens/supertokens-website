6a22b4f6930e0dd6525bbcb00e8d49d15b2cc3b0
Nemi Shah <nemishah1212@gmail.com>
2021-09-29 17:06:04 +0530

    Bump version to 8.2.3

--- lib/ts/version.ts
+++ lib/ts/version.ts
@@ -12,6 +12,6 @@
  * License for the specific language governing permissions and limitations
  * under the License.
  */
-export const package_version = "8.2.2";
+export const package_version = "8.2.3";
 
 export const supported_fdi = ["1.8", "1.9"];
