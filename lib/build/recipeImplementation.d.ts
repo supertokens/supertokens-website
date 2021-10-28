ce8217589793185fb06e47331fba5c7c50612d3a
Rishabh <rishabh@supertokens.io>
2021-10-28 18:17:06 +0530

    removes all arrow functions in recipe as per https://github.com/supertokens/supertokens-node/issues/199

--- lib/build/recipeImplementation.d.ts
+++ lib/build/recipeImplementation.d.ts
@@ -1,9 +1,2 @@
-import { RecipeInterface, NormalisedInputType } from "./types";
-export default class RecipeImplementation implements RecipeInterface {
-    addFetchInterceptorsAndReturnModifiedFetch: (originalFetch: any, _: NormalisedInputType) => typeof fetch;
-    addAxiosInterceptors: (axiosInstance: any, _: NormalisedInputType) => void;
-    getUserId: (_: NormalisedInputType) => Promise<string>;
-    getAccessTokenPayloadSecurely: (config: NormalisedInputType) => Promise<any>;
-    doesSessionExist: (_: NormalisedInputType) => Promise<boolean>;
-    signOut: (config: NormalisedInputType) => Promise<void>;
-}
+import { RecipeInterface } from "./types";
+export default function RecipeImplementation(): RecipeInterface;
