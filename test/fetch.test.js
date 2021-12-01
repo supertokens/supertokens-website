905ec529a60ff733e5929accb24283aa0a555b6a
Rishabh <rishabh@supertokens.io>
2021-12-01 18:49:28 +0530

    fixes to some tsts

--- test/fetch.test.js
+++ test/fetch.test.js
@@ -522,8 +522,6 @@ describe("Fetch AuthHttpRequest class tests", function() {
             await page.evaluate(async () => {
                 let BASE_URL = "http://localhost.org:8080";
 
-                let jwtEnabled = (await (await fetch(BASE_URL + "/featureFlags")).json()).sessionJwt;
-
                 supertokens.init({
                     apiDomain: BASE_URL
                 });
@@ -543,16 +541,7 @@ describe("Fetch AuthHttpRequest class tests", function() {
 
                 let data = await supertokens.getAccessTokenPayloadSecurely();
 
-                if (!jwtEnabled) {
-                    assertEqual(Object.keys(data).length, 0);
-                } else {
-                    assertEqual(Object.keys(data).length, 5);
-                    assertNotEqual(data.jwt, undefined);
-                    assertEqual(data._jwtPName, "jwt");
-                    assertEqual(data.customClaim, "customValue");
-                    assertEqual(data.sub, userId);
-                    assertEqual(data.iss, "http://0.0.0.0:8080");
-                }
+                assertEqual(Object.keys(data).length, 0);
 
                 // update jwt data
                 let testResponse1 = await fetch(`${BASE_URL}/update-jwt`, {
