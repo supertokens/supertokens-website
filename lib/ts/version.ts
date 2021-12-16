561ae6f600504910ae5e71a85abcc0b8913094e1
Rishabh <rishabh@supertokens.io>
2021-12-16 13:31:19 +0530

    bumps version for fdi 1.11 support

--- lib/ts/version.ts
+++ lib/ts/version.ts
@@ -12,6 +12,6 @@
  * License for the specific language governing permissions and limitations
  * under the License.
  */
-export const package_version = "10.0.5";
+export const package_version = "10.0.6";
 
-export const supported_fdi = ["1.8", "1.9", "1.10"];
+export const supported_fdi = ["1.8", "1.9", "1.10", "1.11"];
