c83e8c7e172184fb738739046af8c7634c9b1433
Nemi Shah <nemishah1212@gmail.com>
2022-06-13 06:24:41 +0000

    Remove sync functions from cookie handler

--- lib/build/utils/cookieHandler/types.d.ts
+++ lib/build/utils/cookieHandler/types.d.ts
@@ -1,19 +1,8 @@
 /**
- * When using this library with frameworks where cookie management
- * requires async handling (react-native for example) we use `getCookie`
- * and `setCookie` which are async.
- *
- * When used in cases where we need to use cookies in a synchronous way
- * (supertokens-auth-react reads cookies when rendering the UI) we use the
- * sync functions instead.
+ * Refer to this issue to know why this is required: https://github.com/supertokens/supertokens-website/issues/134
  */
 export declare type CookieHandlerInterface = {
     setCookie: (cookieString: string) => Promise<void>;
     getCookie: () => Promise<string>;
-    /**
-     * Sync versions of the functions
-     */
-    setCookieSync: (cookieString: string) => void;
-    getCookieSync: () => string;
 };
 export declare type CookieHandlerInput = (original: CookieHandlerInterface) => CookieHandlerInterface;
