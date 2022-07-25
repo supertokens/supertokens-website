d279d67a92b5205348b55c684e8e9ceabf45f0c9
Mihály Lengyel <mihaly@lengyel.tech>
2022-07-25 09:26:31 +0200

    feat(sessionclaims): add primitive array claim (#145)
    * feat(sessionclaims): add primitive array claim to support roles claim
    
    * feat(sessionclaims): removed strictEquals
    
    * fix(sessionclaims): make maxAgeInSeconds optional in in/excludesAll validators

---
+++ lib/build/claims/primitiveArrayClaim.d.ts
@@ -0,0 +1,18 @@
+import { SessionClaimValidator } from "../types";
+export declare type PrimitiveArrayClaimConfig = {
+    id: string;
+    refresh: (userContext?: any) => Promise<void>;
+};
+export declare class PrimitiveArrayClaim<ValueType> {
+    readonly id: string;
+    readonly refresh: SessionClaimValidator["refresh"];
+    constructor(config: PrimitiveArrayClaimConfig);
+    getValueFromPayload(payload: any, _userContext?: any): ValueType[];
+    getLastFetchedTime(payload: any, _userContext?: any): number | undefined;
+    validators: {
+        includes: (val: ValueType, maxAgeInSeconds?: number | undefined, id?: string | undefined) => SessionClaimValidator;
+        excludes: (val: ValueType, maxAgeInSeconds?: number | undefined, id?: string | undefined) => SessionClaimValidator;
+        includesAll: (val: ValueType[], maxAgeInSeconds?: number | undefined, id?: string | undefined) => SessionClaimValidator;
+        excludesAll: (val: ValueType[], maxAgeInSeconds?: number | undefined, id?: string | undefined) => SessionClaimValidator;
+    };
+}
