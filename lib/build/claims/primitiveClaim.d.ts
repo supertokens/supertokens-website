4fb3dbfaf5aba945dee0e2bccc01e4f967742af5
Mihály Lengyel <mihaly@lengyel.tech>
2022-10-14 07:50:58 +0200

    feat: remove default max age claims (#162)
    * feat: remove default max age from claims
    
    * chore: update changelog

--- lib/build/claims/primitiveClaim.d.ts
+++ lib/build/claims/primitiveClaim.d.ts
@@ -7,11 +7,11 @@ export declare type PrimitiveClaimConfig = {
 export declare class PrimitiveClaim<ValueType> {
     readonly id: string;
     readonly refresh: SessionClaimValidator["refresh"];
-    readonly defaultMaxAgeInSeconds: number;
+    readonly defaultMaxAgeInSeconds: number | undefined;
     constructor(config: PrimitiveClaimConfig);
     getValueFromPayload(payload: any, _userContext?: any): ValueType;
     getLastFetchedTime(payload: any, _userContext?: any): number | undefined;
     validators: {
-        hasValue: (val: ValueType, maxAgeInSeconds?: number, id?: string) => SessionClaimValidator;
+        hasValue: (val: ValueType, maxAgeInSeconds?: number | undefined, id?: string) => SessionClaimValidator;
     };
 }
