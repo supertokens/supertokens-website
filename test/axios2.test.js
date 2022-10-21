7ea18818a9a38db8d1fb9ec75ed9469477724e6f
rishabhpoddar <rishabh.poddar@gmail.com>
2022-10-21 14:22:38 +0530

    small change to test

--- test/axios2.test.js
+++ test/axios2.test.js
@@ -1277,8 +1277,7 @@ describe("Axios AuthHttpRequest class tests", function() {
     it("test when ACCESS_TOKEN_PAYLOAD_UPDATED is fired", async function() {
         await startST(3);
         const browser = await puppeteer.launch({
-            args: ["--no-sandbox", "--disable-setuid-sandbox"],
-            headless: false
+            args: ["--no-sandbox", "--disable-setuid-sandbox"]
         });
 
         try {
