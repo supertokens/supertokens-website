3d46dd0c3a8cd06644b72b12f5ce374a082fb0da
Rishabh Poddar <rishabh.poddar@gmail.com>
2023-04-25 21:00:40 +0530

    Storage handler for browser-tabs-lock (#213)
    * initial commit
    
    * fixes dependency

--- lib/ts/utils/lockFactory/index.ts
+++ lib/ts/utils/lockFactory/index.ts
@@ -1,19 +1,20 @@
 import { LockFactory } from "./types";
 import Lock from "browser-tabs-lock";
+import { StorageHandler } from "../windowHandler/types";
 
-const defaultFactory = () => Promise.resolve(new Lock());
+const defaultFactory = (storageHandler?: StorageHandler) => () => Promise.resolve(new Lock(storageHandler));
 
 export default class LockFactoryReference {
     private static instance?: LockFactoryReference;
 
     constructor(public lockFactory: LockFactory) {}
 
-    static init(lockFactory?: LockFactory) {
+    static init(lockFactory?: LockFactory, storageHandler?: StorageHandler) {
         // This is copied from the other XXXReference clasess
         if (this.instance !== undefined) {
             return;
         }
-        this.instance = new LockFactoryReference(lockFactory ?? defaultFactory);
+        this.instance = new LockFactoryReference(lockFactory ?? defaultFactory(storageHandler));
     }
 
     static getReferenceOrThrow() {
