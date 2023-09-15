3741888310db45c2257f40bc1fbfac380ccb6985
Mihaly Lengyel <mihaly@lengyel.tech>
2023-09-15 10:16:07 +0200

    chore: bump version

--- lib/ts/version.ts
+++ lib/ts/version.ts
@@ -12,6 +12,6 @@
  * License for the specific language governing permissions and limitations
  * under the License.
  */
-export const package_version = "17.0.2";
+export const package_version = "17.0.3";
 
 export const supported_fdi = ["1.16", "1.17", "1.18"];
