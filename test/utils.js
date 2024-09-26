a11765cf5e2423b0e231f8ef58ddf25d20a4d38c
Mihaly Lengyel <mihaly@lengyel.tech>
2024-09-27 01:15:14 +0200

    ci: support running test from custom node/core branches

--- test/utils.js
+++ test/utils.js
@@ -99,7 +99,9 @@ module.exports.addBrowserConsole = function (page) {
 
 module.exports.coreTagEqualToOrAfter = function (targetTag) {
     const currTag = process.env.SUPERTOKENS_CORE_TAG;
-    if (currTag === undefined || currTag === targetTag) return true;
+    if (currTag === undefined || currTag === targetTag || (!currTag.startsWith("dev-v") && targetTag.startsWith("v"))) {
+        return true;
+    }
 
     const lparts = currTag.replace(/^(dev-)?v/, "").split(".");
     while (lparts.length < 3) lparts.push("0");
